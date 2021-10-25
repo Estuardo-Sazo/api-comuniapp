294429df4c462befd613d2c222b5595e0d32a36f
Estuardo-sazo <estuardosazo1206@gmail.com>
2021-10-24 22:16:34 -0600

    cambio de puerto

--- classes/server.ts
+++ classes/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 export default class Server {
     public app: express.Application;
-    public port: number = 3001;
+    public port: number = 3000;
 
     constructor() {
         this.app = express();
